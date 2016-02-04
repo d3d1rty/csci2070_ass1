434980c2b932291b1a757339af1c646a1c29bf00
Dick Davis <rdavis89@my.apsu.edu>
2016-02-04 15:31:47 -0600

    finished q1

---
+++ RichardDavisPersonal.java
@@ -0,0 +1,21 @@
+/**
+ * This program prints basic biographical data.
+ * Author: Richard Davis
+ */
+public class RichardDavisPersonal
+{
+  public static void main(String[] args)
+  {
+    String name = "Richard Davis";
+    String address = "123 Main St, Anytown, USA 37040";
+    String phone = "931-555-1234";
+    String major = "Information Systems";
+
+    System.out.println(name + "\n" +
+                       address + "\n" +
+                       phone + "\n" +
+                       major);
+
+    System.exit(0);
+  }
+}
