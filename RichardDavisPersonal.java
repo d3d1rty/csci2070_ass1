3064a8570381dd5ab45440ae16b16f96dfb64a0b
Dick Davis <rdavis89@my.apsu.edu>
2016-02-04 19:25:30 -0600

    added commenting to q1

--- RichardDavisPersonal.java
+++ RichardDavisPersonal.java
@@ -6,16 +6,18 @@ public class RichardDavisPersonal
 {
   public static void main(String[] args)
   {
-    String name = "Richard Davis";
-    String address = "123 Main St, Anytown, USA 37040";
-    String phone = "931-555-1234";
-    String major = "Information Systems";
+    String name = "Richard Davis"; // stores name in a string
+    String address = "123 Main St, Anytown, USA 37040"; // stores address in a string
+    String phone = "931-555-1234"; // stores phone number in a string
+    String major = "Information Systems"; // stores major in a string
 
+    // prints biographical information separated by newline characters
     System.out.println(name + "\n" +
                        address + "\n" +
                        phone + "\n" +
                        major);
 
+    // terminates program
     System.exit(0);
   }
 }
