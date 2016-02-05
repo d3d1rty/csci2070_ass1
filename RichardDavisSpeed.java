7fb919a801fa32aae079426b0b4030153cfecaaa
Dick Davis <rdavis89@my.apsu.edu>
2016-02-04 23:02:24 -0600

    started q6

---
+++ RichardDavisSpeed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * This program calculates the time it takes for a sound wave to travel a given distance
+ * through several different mediums.
+ * Author: Richard Davis
+ */
+
+public class RichardDavisSpeed
+{
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in); // creates a scanner object
+
+    // 
+
+
+    // terminates program
+    System.exit(0);
+  }
+}
