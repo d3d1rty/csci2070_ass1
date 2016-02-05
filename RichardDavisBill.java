55a4f6894f2b95032de49704bc06033579fb8b77
Dick Davis <rdavis89@my.apsu.edu>
2016-02-04 22:13:16 -0600

    started writing q5

---
+++ RichardDavisBill.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * This program calculates the cost for a restaraunt bill.
+ * Author: Richard Davis
+ */
+
+public class RichardDavisBill
+{
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in); // creates a scanner object
+    
+    // terminates program
+    System.exit(0);
+  }
+}
